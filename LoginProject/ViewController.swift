5b54763209f37c9315072ecf885abff344a74d71
yw22 <youngwook.dev@gmail.com>
2022-05-21 15:37:55 +0900

    Init : "초기 설정"

--- LoginProject/ViewController.swift
+++ LoginProject/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let emailTextFieldView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        makeUI()
+        
     }
 
 
+    
+    func makeUI() {
+        emailTextFieldView.backgroundColor = .darkGray
+        view.addSubview(emailTextFieldView)
+        
+        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false //
+        
+        // 왼쪽
+        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        // 오른쪽
+        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        // 위
+        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        // 높이
+        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
 }
 
